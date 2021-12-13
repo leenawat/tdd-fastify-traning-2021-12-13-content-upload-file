742544a7232928ca9d198fb4806b89872c2c73a0
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-13 17:30:37 +0700

    server.js and jest

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify'
+import fp from 'fastify-plugin'
+import App from './app'
+
+const port = 8080
+
+const start = async () => {
+  try {
+    const app = fastify({
+      logger: {
+        level: 'info',
+      },
+      ajv: {
+        customOptions: {
+          allErrors: true,
+        },
+      },
+      disableRequestLogging: true,
+    })
+    app.register(fp(App))
+    await app.listen(port)
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
+}
+
+start()
