04fbe202a957ff86ebff56eeb31e514bcb4d0784
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-14 02:10:53 +0700

    check file ที่ upload ว่ามีอยู่จริง

--- src/routes/FileUpload.ts
+++ src/routes/FileUpload.ts
@@ -1,10 +1,41 @@
+/* eslint-disable no-unused-vars */
 import { FastifyPluginAsync } from 'fastify'
 import multer from 'fastify-multer'
 import { File } from 'fastify-multer/lib/interfaces'
-// import fs from 'fs'
+// import { fileTypeFromBuffer } from 'file-type'
+// import * as fs from 'fs'
+import * as path from 'path'
+import { v4 as uuidv4 } from 'uuid'
 
 const FileUpload: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  const upload = multer({ dest: 'uploads/' })
+  const uploadPath = 'uploads/'
+  // const fileAmountLimit = 3
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, uploadPath)
+    },
+    filename: (req, file, cb) => {
+      const _ext = path.extname(file.originalname)
+      const filename = uuidv4() + _ext
+      cb(null, filename)
+    },
+  })
+  // const upload = multer({ dest: uploadPath })
+  const upload = multer({
+    storage,
+    limits: {
+      fileSize: 5 * 1024 * 1024,
+    },
+    fileFilter: (req, file, cb) => {
+      // กำหนด type
+      if (!(file.mimetype == 'image/png' || file.mimetype == 'image/jpeg')) {
+        return cb(new Error('Invalid mimetype!'), false)
+      }
+      cb(null, true)
+    },
+  })
+
   fastify.post('/api/upload', {
 
     preHandler: upload.single('file'),
--- test/routes/FileUpload.test.ts
+++ test/routes/FileUpload.test.ts
@@ -1,6 +1,7 @@
 import { build } from '../helper'
 import formMethod from 'form-auto-content'
-import fs from 'fs'
+import * as fs from 'fs'
+import * as path from 'path'
 
 describe('upload file tests', () => {
   const app = build()
@@ -20,5 +21,25 @@ describe('upload file tests', () => {
     })
     expect(res.statusCode).toBe(200)
   })
+
+  it.each`
+    file              | fileExtension
+    ${'test-png.png'} | ${'png'}
+  `(
+    'saves filename with extension $fileExtension in attachment object and stored object when $file is uploaded',
+    async ({ fileExtension, file }) => {
+      const form:any = formMethod({
+        // foo: 'bar',
+        file: fs.createReadStream('./test/resources/test-png.png'),
+      })
+      const res:any = await app.inject({
+        method: 'post',
+        url: '/api/upload',
+        ...form,
+      })
+      const filePath = path.join('.', 'uploads', res.json().filename)
+      expect(fs.existsSync(filePath)).toBe(true)
+    },
+  )
 })
 
